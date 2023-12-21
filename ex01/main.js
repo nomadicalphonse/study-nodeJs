b4a57c003600ca3a1a402406ec29c24a62b7ce87
alphonse <sos6871comm@gmail.com>
2023-12-21 17:53:50 +0900

    init

--- ex01/main.js
+++ ex01/main.js
@@ -1,1 +1,15 @@
-const http = require("http")
+//슈퍼바이져 설치확인 - npm list -g supervisor
+//슈퍼바이져 설치 - npm install -g supervisor
+//파워쉘권한 - Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+const express = require("express")
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("기본 페이지")
+})
+
+app.get("/test", (req, res)=>{
+    res.send("test 페이지")
+})
+
+app.listen( 3000, ()=>console.log("3000 port server") )
