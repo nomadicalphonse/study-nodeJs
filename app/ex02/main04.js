5f33c2f07b1e874f620d212ff7ba30665ac5a535
alphonse <sos6871comm@gmail.com>
2023-12-25 14:39:42 +0900

    cm

--- app/ex02/main04.js
+++ app/ex02/main04.js
@@ -6,13 +6,11 @@ app.set("views", "./") //ejs파일은 현재위치에서 찾아주세요.
 app.set("view engine", ".ejs"); // view로 사용할 엔진은 ejs파일은 ejs를 사용하겠다는 선언
 
 app.get("/", (req, res)=>{
-    res.render("main03_index", {key:"EX001"})
+    res.render("main04_index")
 })
 
 app.get("/test", (req, res)=>{
-    const name = "홍길동"
-    const arr = ['하나', '두울', "세엣"]
-    const map = {name:name, age:20, list:arr}
-    res.render("main03_test", {n:name, arr:arr, map:map})
+    const prm = {name:'홍길동', age:20}
+    res.render("main04_test", {prm:prm})
 })
 app.listen( 3000, ()=>console.log("3000 port server") )
