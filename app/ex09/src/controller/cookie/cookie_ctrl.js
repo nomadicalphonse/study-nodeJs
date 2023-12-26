f1ae6d3b2bdc47812f57a80d80ed92c54cefb429
alphonse <sos6871comm@gmail.com>
2023-12-26 21:42:10 +0900

    c

--- app/ex09/src/controller/cookie/cookie_ctrl.js
+++ app/ex09/src/controller/cookie/cookie_ctrl.js
@@ -1,11 +1,9 @@
 const cookieConfig = {
     httpOnly : true, //웹통신 할때만 쿠키를 적용
-    maxAge : 5000 //5초동안 쿠키를 유히
+    maxAge : 10000 //10초동안 쿠키를 유히
 }
 
 const index = (req, res) => {
-    //res.send("컨트롤러 연동")
-    //res.cookie("myCookie", "valueCookie")
     const userCookie = req.cookies.myCookie
     console.log("user cookie : ", userCookie)
 
@@ -13,4 +11,15 @@ const index = (req, res) => {
     res.cookie("myCookie", "valueCookie", cookieConfig)
     res.render("cookie/cookie01", {userCookie})
 }
-module.exports = {index}
+const popup = (req, res) => {
+    const userCookie = req.cookies.myCookie
+    res.render("cookie/popup", {userCookie})
+}
+const cookie01 = (req, res) => {
+    res.render("cookie/cookie01")
+}
+const makeCookie = (req, res) => {
+    res.cookie("myCookie", "value", cookieConfig)
+    res.send("<script>window.close()</script>")
+}
+module.exports = {index, popup, cookie01, makeCookie}
--- app/ex09/src/routes/cookie/cookie_router.js
+++ app/ex09/src/routes/cookie/cookie_router.js
@@ -2,9 +2,7 @@ const express = require("express")
 const router = express.Router()
 const ctrl = require("../../controller/cookie/cookie_ctrl")
 router.get("/", ctrl.index)
-/*
-router.get("/", (req, res)=>{
-    res.send("연동 성공")
-})
-*/
+router.get("/popup", ctrl.popup)
+router.get("/cookie01", ctrl.cookie01)
+router.get("/makeCookie", ctrl.makeCookie)
 module.exports = router
